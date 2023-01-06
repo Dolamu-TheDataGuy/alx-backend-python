a76fab6bf5e1f4d0901a20b0208eac4841009071
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-01-06 06:16:41 +0100

    added multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Multiplier Annotated Function"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """a type-annotated function make_multiplier that takes a float multiplier
+    as an argument and returns a function that multiplies a float by a multiplier"""
+    return multiplier * multiplier
+

de3558657607ea5be9e313478284685baf161172
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-01-06 11:27:04 +0100

    added revamps

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ Working with Python type annotations """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Takes a list of floats as input and return the sum
     as a float
     """
-    return float(sum(input_list))
+    return sum(input_list)

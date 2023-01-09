1defbd02069d2b6d25e55eebc969711568342b3b
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-01-09 13:32:48 +0100

    added readme

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 """Multiplier Annotated Function"""
+
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Multiplies a float by multiplier
-    Args:
-        multiplier (float): The multiplier
-    Returns:
-        A function that multiplies a float by multiplier
-    """
-    def multiply(num: float) -> float:
-        """Multiplies a float by a multiplier"""
+    """a type-annotated function make_multiplier that takes a float multiplier
+    as an argument and returns a function that multiplies a float by a multiplier"""
+    def multiply(num: float) -> Callable[[float], float]:
         return multiplier * num
-
     return multiply
+    
